f8c831ee74d84cd65813d6b1d7dd04d18cbab18f
Rossibelas <guilhermerossi103@gmail.com>
2023-09-05 22:15:59 -0300

    pegar o nome

---
+++ app.js
@@ -0,0 +1,25 @@
+// comando de saída de dados
+/*
+alert("oi, isso é uma mensagem")
+
+console.log("Olá, isso é outra mensagem")
+
+alert (10/5)
+
+console.log(10/5)
+*/
+
+nota1 = 10
+nota2 = 8
+nota3 = 5
+nota4 = 8 
+
+console.log("A média do joão é:")
+// console.log((nota1 + nota2 + nota3 + nota4)/4)
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log(media)
+
+nome = prompt("Informe seu nome")
+alert("Olá " + nome + " sua média é " + media)
